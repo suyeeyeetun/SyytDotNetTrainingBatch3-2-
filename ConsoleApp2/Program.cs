985f80e762305b224144aa8ab4d8a20cdddb66b3
suyeeyeetun <Suyeeyeetun15@gmail.com>
2025-10-01 22:31:48 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "."; 
+sqlConnectionStringBuilder.InitialCatalog = "Batch3MiniPOS";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sasa@123";
+sqlConnectionStringBuilder.TrustServerCertificate = true;
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+
+connection.Open();
+
+string query = @"SELECT [ProductId]
+      ,[ProductName]
+      ,[Price]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Product]";
+
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt); // execute the query and fill the DataTable
+
+connection.Close();
+
+for (int i = 0; i < dt.Rows.Count; i++)
+{
+    DataRow row = dt.Rows[i];
+    int rowNo = i + 1;
+    decimal price = Convert.ToDecimal(row["Price"]);
+    Console.WriteLine(rowNo.ToString() + ". " + row["ProductName"] + "(" + price.ToString("n0") + ")");
+    
+}
+
+Console.ReadLine();
